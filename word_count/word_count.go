8b27cec9861da3ee0cfa77eb1c5600a49c52b3de
Yerzhan Karatayev <omnilumiere@gmail.com>
2023-12-04 01:37:49 +0600

    added updated function and tests

--- word_count/word_count.go
+++ word_count/word_count.go
@@ -7,31 +7,20 @@ import (
 
 func main() {
 	sampleString := "count each word in in in in the the string"
-
 	counter := WordCount(sampleString)
 
-	fmt.Println(counter)
+	for key, value := range counter {
+		fmt.Printf("%v: %v\n", key, value)
+	}
 }
 
 func WordCount(stroke string) map[string]int {
+	slice := strings.Fields(stroke)
 	wordCount := make(map[string]int)
 
-	slice := strings.Split(stroke, " ")
-	fmt.Println(slice)
-
 	for _, word := range slice {
-
-		if doesExist(word, stroke) {
-			wordCount[word]++
-		} else {
-			wordCount[word] = 1
-		}
-
+		wordCount[word]++
 	}
+
 	return wordCount
-
-}
-
-func doesExist(word string, stroke string) bool {
-	return strings.Contains(stroke, word)
 }
--- word_count/word_count_test.go
+++ word_count/word_count_test.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestWordCount(t *testing.T) {
-	//TODO: Add TableDriven implementation
+	tests := []struct {
+		input    string
+		expected map[string]int
+	}{
+		{
+			input: "count each word in in in in the the string",
+			expected: map[string]int{
+				"count":  1,
+				"each":   1,
+				"word":   1,
+				"in":     4,
+				"the":    2,
+				"string": 1,
+			},
+		},
+		{
+			input:    "",
+			expected: map[string]int{},
+		},
+	}
 
-	sampleText := "SOME some CASE caSE"
-
-	expected := make(map[string]int, 0)
-	expected["some"] = 2
-	expected["case"] = 2
-
-	result := WordCount(sampleText)
-
-	//TODO: реализовать проверку результата, вне зависимости от регистра
-
+	for _, test := range tests {
+		result := WordCount(test.input)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("expected: %v, got: %v", test.expected, result)
+		}
+	}
 }
