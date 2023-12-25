2610d4bedc7f289edc15bd88aaa6ac2e3381bb5a
Yerzhan Karatayev <omnilumiere@gmail.com>
2023-12-26 01:01:34 +0600

    added indexes for employee columns

---
+++ database/migration/000002_indexes.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX idx_firstname;
+DROP INDEX idx_lastname;
+DROP INDEX idx_firstname_lastname;
---
+++ database/migration/000002_indexes.up.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_firstname ON employee (first_name);
+CREATE INDEX idx_lastname ON employee (last_name);
+CREATE INDEX idx_firstname_lastname ON employee (first_name, last_name);
