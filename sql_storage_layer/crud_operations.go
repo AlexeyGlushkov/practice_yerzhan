6d73c948ba4a64bab6302b0c978abe573f64e88c
Yerzhan Karatayev <yerzhan.karatayev@ventionteams.com>
2023-12-27 16:54:25 +0600

    added create operation in transaction

--- sql_storage_layer/crud_operations.go
+++ sql_storage_layer/crud_operations.go
@@ -1,42 +1,26 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-type Storage interface {
-	Create(data interface{}) error
-	Read(id int) (interface{}, error)
-	Update(id int, newData interface{}) error
-	Delete(id int) error
-}
-
-type Employee struct {
-	Employee_id int
-	First_name  string
-	Last_name   string
-	Position_id string
-}
-
 type EmployeeRepository struct {
 	DB *sql.DB
-}
-
-type Position struct {
-	Position_id   string
-	Position_name string
-	Salary        int
 }
 
 type PositionRepository struct {
 	DB *sql.DB
 }
 
-func (er *EmployeeRepository) Create(employee Employee) error {
-	_, err := er.DB.Exec("INSERT INTO employee (employee_id, first_name, last_name, position_id) VALUES ($1, $2, $3, $4)",
+func (er *EmployeeRepository) CreateEmployee(employee Employee) error {
+
+	_, err := er.DB.Exec("INSERT INTO employee (employee_id, first_name, last_name, position_id)"+
+		"VALUES ($1, $2, $3, $4)",
 		employee.Employee_id, employee.First_name, employee.Last_name, employee.Position_id)
 
 	if err != nil {
@@ -44,4 +28,33 @@ func (er *EmployeeRepository) Create(employee Employee) error {
 		return err
 	}
 	return nil
+}
+
+func (pr *PositionRepository) CreatePosition(ctx context.Context, position Position) (positionID string, err error) {
+
+	fail := func(err error) (string, error) {
+		return "", fmt.Errorf("CreatePosition: %v", err)
+	}
+
+	tx, err := pr.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return fail(err)
+	}
+	defer tx.Rollback()
+
+	var position_id string
+	if err = tx.QueryRowContext(ctx, "INSERT INTO position (position_name, salary)"+
+		" VALUES ($1, $2) RETURNING position_id", position.Position_name, position.Salary).Scan(&position_id); err != nil {
+		if err == sql.ErrNoRows {
+			return fail(fmt.Errorf("no rows returned: %v", err))
+		}
+		return fail(fmt.Errorf("error inserting position: %v", err))
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fail(err)
+	}
+
+	return position_id, nil
+
 }
---
+++ sql_storage_layer/entities.go
@@ -0,0 +1,14 @@
+package main
+
+type Employee struct {
+	Employee_id int
+	First_name  string
+	Last_name   string
+	Position_id string
+}
+
+type Position struct {
+	Position_id   string
+	Position_name string
+	Salary        int
+}
